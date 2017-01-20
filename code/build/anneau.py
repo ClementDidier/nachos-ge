9bb129c2d05f4b2c264c90dbefa89ca0bddbde8d
Fl0rc <florent.chastagner@gmail.com>
2017-01-20 09:37:25 +0100

    Etape 6 Ajout du script python à git

---
+++ code/build/anneau.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+import multiprocessing
+
+def f(i):
+	subprocess.call(["./nachos-network", "-m", str(i), "-h", str((i+1)%int(n))])
+
+l = []
+n = sys.argv[1]
+i = 0
+while (i < int(n)):
+	p = multiprocessing.Process(target = f, args = (i,))
+	l.append(p)
+	p.start()
+	i = i + 1
+for p in l:
+	p.join()
